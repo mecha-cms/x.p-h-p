783823ad8a827eca0ac5b47127e367725eb32586
Taufik Nurrohman <t.nurrohman77@gmail.com>
2020-07-17 09:59:30 +0700

    Update

--- README.md
+++ README.md
@@ -4,6 +4,10 @@ PHP Page Extension for Mecha
 Release Notes
 -------------
 
+### 1.0.2
+
+ - Added ability to evaluate alternative block syntax. Example: `<?php if ($a): ?><b>c</b><?php endif; ?>`.
+
 ### 1.0.1
 
  - Added page type field with a value of `PHP` for [panel](https://github.com/mecha-cms/x.panel) extension.
--- p-h-p/index.php
+++ p-h-p/index.php
@@ -19,6 +19,7 @@ function p_h_p($content) {
             extract($GLOBALS, \EXTR_SKIP);
             // Parse mixed PHP expression and plain HTML data
             $parts = \preg_split('/(<\?(?:php|=)(?:[\s\S]+?)(?:\?>|$))/', $content, null, \PREG_SPLIT_NO_EMPTY | \PREG_SPLIT_DELIM_CAPTURE);
+            $out = "";
             foreach ($parts as $part) {
                 $e = 0 === \strpos($part, '<?='); // Echo?
                 if (0 === \strpos($part, '<?php') || $e) {
@@ -26,11 +27,15 @@ function p_h_p($content) {
                     if ('?>' === \substr($part, -2)) {
                         $part = \substr($part, 0, -2);
                     }
-                    eval(($e ? 'echo ' : "") . \trim($part));
+                    if (';' !== \substr($part = \trim($part), -1)) {
+                        $part .= ';';
+                    }
+                    $out .= ($e ? ' echo ' : "") . $part;
                 } else {
-                    echo $part;
+                    $out .= " echo '" . \strtr($part, ["'" => "\\'"]) . "';";
                 }
             }
+            eval($out);
             return \ob_get_clean();
         }, [$content], $this);
     }
